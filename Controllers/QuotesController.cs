dbe170ee047db94497884e2776b1f12b02fb3750
Oybek Abdullaev <oabdullaev@appulate.com>
2021-08-28 13:28:04 +0500

    create quote entity and controller

---
+++ Controllers/QuotesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Quotes.Controllers
+{
+    [ApiController]
+    [Route("quotes")]
+    public class QuotesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Quote> GetAll()
+        {
+            return new List<Quote>
+            {
+                new Quote
+                {
+                    Id = 1,
+                    Author = "Author 1",
+                    Body = "Body 1",
+                    Category = "Category 1"
+                },
+                new Quote
+                {
+                    Id = 2,
+                    Author = "Author 2",
+                    Body = "Body 2",
+                    Category = "Category 2"
+                }
+            };
+        }
+    }
+}
---
+++ Quote.cs
@@ -0,0 +1,10 @@
+﻿namespace Quotes
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Body { get; set; }
+        public string Category { get; set; }
+    }
+}
